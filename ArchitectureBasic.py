717bac7411d7579198e1960d311c795fc07cc50b
jdavis-132 <jdavis132@huskers.unl.edu>
2023-10-03 15:00:03 -0500

    Initial architecture and model training code for basic model

--- ArchitectureBasic.py
+++ ArchitectureBasic.py
@@ -13,11 +13,27 @@ import PIL
 import Dataset as dl
 
 class architectureBasic(nn.Module):
-    def __init__(self, **kwargs):
-        super.init()
-        self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 16, kernel_size = (5, 5), padding = 1)
-        self.activation = nn.SELU()
-        self.linear1 = nn.Linear(kwargs['input_shape'], 2200)
-        self.linear2 = nn.Linear(2200, 3000)
-        self.linear3 = nn.Linear(3000, 2024)
-        self.linear4 = nn.Linear(2024, kwargs['output_shape'])
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels = 3, out_channels = 16, kernel_size = (5, 5), padding = 1)
+        self.linear1 = nn.Linear(4000, 2000)
+        self.linear2 = nn.Linear(2000, 1000)
+        self.linear3 = nn.Linear(1000, 500)
+        self.linear4 = nn.Linear(500, 250)
+        self.linear5 = nn.Linear(250, 100)
+        self.linear6 = nn.Linear(100, 50)
+        self.linear7 = nn.Linear(50, 1)
+
+    def forward(self, x):
+        conv = self.conv(x)
+        flatten = conv.view(-1)
+        linear1 = F.relu(self.linear1(flatten))
+        linear2 = F.relu(self.linear2(linear1))
+        linear3 = F.relu(self.linear3(linear2))
+        linear4 = F.relu(self.linear4(linear3))
+        linear5 = F.relu(self.linear5(linear4))
+        linear6 = F.relu(self.linear6(linear5))
+        linear7 = F.tanhshrink(self.linear7(linear6))
+        
+        return linear7
+        
---
+++ mainBasic.py
@@ -0,0 +1,38 @@
+import Dataset as dat
+import ArchitectureBasic as arch
+import numpy as np
+import torch
+import torch.nn as nn
+
+dataset = dat.satelliteimage_dataset(image_path = 'imagedata', label_path = 'labelfolder/label.csv')
+trainSize = int(dataset.__len__() * 0.8)
+testSize = dataset.__len__() - trainSize
+trainData, testData = torch.utils.data.random_split(dataset, [trainSize, testSize])
+trainLoader=torch.utils.data.DataLoader(dataset=trainData, batch_size=1, shuffle=True)
+testLoader=torch.utils.data.DataLoader(dataset=testData, batch_size=1, shuffle=True)
+
+model = arch.architectureBasic()
+optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
+numEpochs = 10
+totalLoss = []
+
+for epoch in range(numEpochs):
+    
+    epochLoss = []
+    
+    for batch_idx, (data, label) in enumerate(trainLoader):
+        
+        currModel = model(data)
+        optimizer.zero_grad()
+        loss = nn.MSELoss(currModel, label)
+        epochLoss.append(loss.item())
+        loss.backward()
+        optimizer.step()
+    
+    epochMeanLoss = np.mean(epochLoss)
+    totalLoss.append(epochMeanLoss)
+    print(epochMeanLoss)
+    
+        
+        
+
